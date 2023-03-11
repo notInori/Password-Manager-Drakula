8a42d71114d99e5615edd3f1bb2090194a253a52
ishrainy <nicksu2005@gmail.com>
2023-03-11 00:32:31 +0000

    Fixed Spelling Errors

--- JansUILib/frmMain.vb
+++ JansUILib/frmMain.vb
@@ -374,7 +374,7 @@ Public Class MainProgram
     'Deletes Selected User
     Private Sub DeleteUser(sender As Object, e As EventArgs) Handles BtnDelete.Click, BtnContinueAction.Click, BtnCancelAction.Click
         If sender Is BtnDelete And lbxUsernames.SelectedItem <> Nothing Then
-            lblConfirmationText.Text = "Are you that you want to delete " & lbxUsernames.SelectedItem.ToString & "?"
+            lblConfirmationText.Text = "Are you sure that you want to delete " & lbxUsernames.SelectedItem.ToString & "?"
             pnlConfirmation.Dock = DockStyle.Fill
             pnlConfirmation.BringToFront()
         ElseIf sender Is BtnDelete Then
