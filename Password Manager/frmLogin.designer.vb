660e13a08b21cb3106a6f3b21e4fbb10420b6de8
ishrainy <nicksu2005@gmail.com>
2023-03-13 19:24:58 +0000

    Update Designer

--- Password Manager/frmLogin.designer.vb
+++ Password Manager/frmLogin.designer.vb
@@ -22,6 +22,7 @@ Partial Class AuthLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AuthLogin))
         Me.pnlBackground = New System.Windows.Forms.Panel()
         Me.tblWindow = New System.Windows.Forms.TableLayoutPanel()
@@ -113,7 +114,7 @@ Partial Class AuthLogin
         Me.btnLogin = New System.Windows.Forms.Button()
         Me.btnDummy = New System.Windows.Forms.Button()
         Me.pnlBlackOutline = New System.Windows.Forms.Panel()
-        Me.tmrAnimation = New System.Windows.Forms.Timer()
+        Me.tmrAnimation = New System.Windows.Forms.Timer(Me.components)
         Me.pnlBackground.SuspendLayout()
         Me.tblWindow.SuspendLayout()
         Me.pnlTopBar.SuspendLayout()
