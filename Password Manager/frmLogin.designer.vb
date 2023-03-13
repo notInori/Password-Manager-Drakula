4be94621f69f753720ef53ced4274f7929767f4a
ishrainy <nicksu2005@gmail.com>
2023-03-13 19:43:13 +0000

    Fixed spelling in login notification prompt.

--- Password Manager/frmLogin.designer.vb
+++ Password Manager/frmLogin.designer.vb
@@ -325,11 +325,11 @@ Partial Class AuthLogin
         '
         Me.pnlNotification.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(39, Byte), Integer))
         Me.pnlNotification.Controls.Add(Me.TableLayoutPanel3)
-        Me.pnlNotification.Location = New System.Drawing.Point(0, 279)
+        Me.pnlNotification.Location = New System.Drawing.Point(0, 39)
         Me.pnlNotification.Margin = New System.Windows.Forms.Padding(0)
         Me.pnlNotification.Name = "pnlNotification"
         Me.pnlNotification.Padding = New System.Windows.Forms.Padding(5)
-        Me.pnlNotification.Size = New System.Drawing.Size(494, 10)
+        Me.pnlNotification.Size = New System.Drawing.Size(494, 250)
         Me.pnlNotification.TabIndex = 10
         '
         'TableLayoutPanel3
@@ -636,7 +636,7 @@ Partial Class AuthLogin
         Me.Label6.Padding = New System.Windows.Forms.Padding(0, 0, 5, 5)
         Me.Label6.Size = New System.Drawing.Size(436, 24)
         Me.Label6.TabIndex = 10
-        Me.Label6.Text = "Username or Password is invalid!"
+        Me.Label6.Text = "Username or password is invalid!"
         Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label5
